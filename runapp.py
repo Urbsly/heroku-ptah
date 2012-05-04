906a719572142657d4c51129e460b81b3d0406e4
Michael Bernstein <michael@fandomhome.com>
2012-05-03 18:58:31 -0600

    removed db change from runapp.py

--- runapp.py
+++ runapp.py
@@ -7,6 +7,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:settings.ini', relative_to='.')
     
-    durl = os.environ.get("DATABASE_URL")
-
     serve(app, host='0.0.0.0', port=port)
